81aeb3c2c11840d73b4d059967aed3a015263e4f
Esther <esther191299@gmail.com>
2019-06-18 05:00:47 +0200

    Renaming

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -13,7 +13,7 @@ import javax.swing.Timer;
 public class Gameplay extends JPanel implements KeyListener, ActionListener{
 	
 	
-	private int[] snakeXLength = new int[750];		//arrays om Xpositie en Y positie te bealen
+	private int[] snakeXLength = new int[750];		//arrays om Xpositie en Y positie te bepalen
 	private int[] snakeYLength = new int[750];
 	
 	private boolean up = false;						//variablen  om te controleren welke kant wij opgaan
@@ -22,20 +22,20 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 	private boolean right = false;
 	
 	
-	private ImageIcon upmouth;						//variabelen voor de jpeg-koppen
-	private ImageIcon leftmouth;
-	private ImageIcon downmouth;
-	private ImageIcon rightmouth;
+	private ImageIcon faceUp;						//variabelen voor de jpeg-koppen
+	private ImageIcon faceDown;
+	private ImageIcon faceLeft;
+	private ImageIcon faceRight;
 	
-	private int lengthOfSnake = 3;					//default length is 3 bolletjes
+	private int snakeLength = 3;					//default length is 3 bolletjes
 
-	private Timer timer;							//bepaalt snelheid slang
+	private Timer speed;							//bepaalt snelheid slang
 	private int delay = 100;
-	private ImageIcon snakeimage;					//variabel voor lichaam: snakebody
+	private ImageIcon snakeBody;					//variabel voor lichaam: snakebody
 	
 	private int moves = 0;
 	
-	private ImageIcon titleImage;					//nieuw object voor image Icon, class title image
+	private ImageIcon icon;							//nieuw object voor image Icon, class Icon
 
 	
 	public Gameplay ()								//De Constructor, (default positie snake/nttrue)
@@ -43,15 +43,15 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 		addKeyListener (this);						//is Gameplay implementatie
 		setFocusable (true);
 		setFocusTraversalKeysEnabled (false);
-		timer = new Timer(delay, this);
-		timer.start();
+		speed = new Timer(delay, this);
+		speed.start();
 	}
 		
 	public void paint (Graphics g)					//Paint method
 	{
 		if(moves == 0)								//Wanneer het spel (opnieuw) start: voer deze positie uit. Wanneer spel bezig is stop
 		{
-			snakeXLength[2] = 50;
+			snakeXLength[2] = 50;                   //bepaalt de positie van elk bolletje: de ketting
 			snakeXLength[1] = 75;
 			snakeXLength[0] = 100;
 			
@@ -65,8 +65,8 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 		g.drawRect(24, 74, 851, 577);
 		
 		// draw the title Image
-		titleImage = new ImageIcon("snaketitle.jpg");
-		titleImage.paintIcon(this, g, 25, 11);
+		icon = new ImageIcon("gameHeader.jpg");
+		icon.paintIcon(this, g, 25, 11);
 		
 		// draw border for gameplay
 		g.setColor(Color.WHITE);
@@ -76,37 +76,37 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 		g.setColor(Color.black);
 		g.fillRect(25, 75, 850, 575);
 		
-		upmouth = new ImageIcon ("rightmouth.png");								//Slang zonder movement/ Snake draw
-		upmouth.paintIcon(this, g, snakeXLength[0], snakeYLength[0]);
+		faceRight = new ImageIcon ("faceRight.png");							//Slang zonder movement/ Snake draw (verander naar right later)
+		faceRight.paintIcon(this, g, snakeXLength[0], snakeYLength[0]);
 		
-		for (int a = 0; a< lengthOfSnake; a++)									//Loop, en in brackets; if-statement om de richting te bepalen
+		for (int a = 0; a< snakeLength; a++)									//Loop, en in brackets; if-statement om de richting te bepalen
 		{
 			
 			if (a==0 && up)
 			{
-				upmouth = new ImageIcon ("upmouth.png");
-				upmouth.paintIcon(this, g, snakeXLength[a], snakeYLength[a]);
+				faceUp = new ImageIcon ("faceUp.png");
+				faceUp.paintIcon(this, g, snakeXLength[a], snakeYLength[a]);
 			}
 			if (a==0 && down)
 			{
-				downmouth = new ImageIcon ("downmouth.png");
-				downmouth.paintIcon(this, g, snakeXLength[a], snakeYLength[a]);
+				faceDown = new ImageIcon ("faceDown.png");
+				faceDown.paintIcon(this, g, snakeXLength[a], snakeYLength[a]);
 			}
 			if (a==0 && left)
 			{
-				leftmouth = new ImageIcon ("leftmouth.png");
-				leftmouth.paintIcon(this, g, snakeXLength[a], snakeYLength[a]);
+				faceLeft = new ImageIcon ("faceLeft.png");
+				faceLeft.paintIcon(this, g, snakeXLength[a], snakeYLength[a]);
 			}
 			if (a==0 && right)
 			{
-				rightmouth = new ImageIcon ("rightmouth.png");
-				rightmouth.paintIcon(this, g, snakeXLength[a], snakeYLength[a]);
+				faceRight = new ImageIcon ("faceRight.png");
+				faceRight.paintIcon(this, g, snakeXLength[a], snakeYLength[a]);
 			}
 			
 			if (a!=0)
 			{
-				snakeimage = new ImageIcon ("snakeimage.png");
-				snakeimage.paintIcon(this, g, snakeXLength[a], snakeYLength[a]);
+				snakeBody = new ImageIcon ("snakeBody.png");
+				snakeBody.paintIcon(this, g, snakeXLength[a], snakeYLength[a]);
 			}
 		}
 		
